fddf14c2da6ec339f6d23619d01f96532c5b5405
Siim Laas <laassiim@gmail.com>
2022-09-04 00:06:02 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// rakendus küsib kasutajal sisestada tema nime
+// rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string- sõne
+string userName = Console.ReadLine();
+// 2 varianti
+//userName = Console.ReadLine();
+
+Console.WriteLine("Hello " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !");
+
